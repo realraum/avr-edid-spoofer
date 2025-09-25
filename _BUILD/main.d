142f37ecbf2989c0717214b6301a5b9356290bfd
Bernhard Tittelbach <bernhard@tittelbach.org>
2025-09-26 00:06:11 +0200

    (re)do. confirmed edid line is checksum ok but laptop does not like it

--- _BUILD/main.d
+++ _BUILD/main.d
@@ -5,5 +5,4 @@ main.o main.d : main.c projInfo.h main.h \
  _commonCode_localized/hfModulation/1.00/hfModulation.h \
  _commonCode_localized/dmsTimer/1.13/dmsTimer.h \
  _commonCode_localized/timerCommon/1.21/timerCommon.h \
- _commonCode_localized/timerCommon/1.21/../../errorHandling/0.99/errorHandling.h \
- _commonCode_localized/sineTable/0.99/sineTable.h
+ _commonCode_localized/timerCommon/1.21/../../errorHandling/0.99/errorHandling.h
